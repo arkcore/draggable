ec6e7c34070d2451ee109fa01c6f5654394b97a9
Vitaly[AVVS] <v@aminev.me>
2015-02-25 19:42:55 +0300

    add main section to package.json, require jQuery shim instead of jQuery module

--- src/jQueryShim.js
+++ src/jQueryShim.js
@@ -1,102 +1,98 @@
 (function (root, factory) {
-	if (typeof exports === 'object') {
-		// Node. Does not work with strict CommonJS, but
-		// only CommonJS-like enviroments that support module.exports,
-		// like Node.
-		module.exports = factory();
-	} else if (typeof define === 'function' && define.amd) {
-		// AMD. Register as an anonymous module.
-		define([], factory);
-	} else {
-		// Browser globals (root is window)
-		root.jQuery = factory();
-	}
+    if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else {
+        // Browser globals (root is window)
+        root.jQuery = factory();
+    }
 }(this, function () {
 
-	'use strict';
+    'use strict';
 
-	var isIE = navigator.appName === 'Microsoft Internet Explorer';
+    var jQuery = function (element) {
 
-	var jQuery = function (element) {
+        return element instanceof Wrap ? element : new Wrap(element);
 
-		return element instanceof Wrap
-			? element
-			: new Wrap (element);
+    };
 
-	};
+    function Wrap(element) {
 
-	function Wrap (element) {
+        this.element = element;
 
-		this.element = element;
+    }
 
-	}
+    // prototype methods
 
-	// prototype methods
+    extend(jQuery, {
 
-	extend(jQuery, {
+        extend: extend
 
-		extend: extend
+    });
 
-	});
+    extend(Wrap.prototype, {
 
-	extend(Wrap.prototype, {
+        on: function (e, fn) {
+            if (e && fn) {
+                this.addEvent(e, fn);
+            } else if (e) {
+                for (var ee in e) {
+                    this.addEvent(ee, e[ee]);
+                }
+            }
+        },
 
-		on: function (e, fn) {
-			if (e && fn) {
-				this.addEvent (e, fn);
-			} else if (e) {
-				for (var ee in e) {
-					this.addEvent (ee, e[ee]);
-				}
-			}
-		},
+        off: function (e, fn) {
+            if (e && fn) {
+                this.removeEvent(e, fn);
+            } else if (e) {
+                for (var ee in e) {
+                    this.removeEvent(ee, e[ee]);
+                }
+            }
+        },
 
-		off: function (e, fn) {
-			if (e && fn) {
-				this.removeEvent (e, fn);
-			} else if (e) {
-				for (var ee in e) {
-					this.removeEvent (ee, e[ee]);
-				}
-			}
-		},
+        addEvent: function (e, fn) {
+						this.element.addEventListener(e, fn, false);
+				},
 
-		addEvent: isIE ?
-			  function (e, fn) { this.element.attachEvent('on'+e, fn) }
-			: function (e, fn) { this.element.addEventListener(e, fn, false) },
+        removeEvent: function (e, fn) {
+            this.element.removeEventListener(e, fn);
+        }
 
-		removeEvent: isIE ?
-			  function (e, fn) { this.element.detachEvent('on'+e, fn) }
-			: function (e, fn) { this.element.removeEventListener(e, fn) }
+    });
 
-	});
+    // helpers
 
-	// helpers
+    function extend() {
 
-	function extend () {
+        var obj = arguments[0];
+        var count = arguments.length;
 
-		var obj = arguments[0];
-		var count = arguments.length;
+        if (!obj) {
+            throw new TypeError('$.extend expects argument 0 to be an Object');
+        }
 
-		if (!obj) {
-			throw new TypeError ('$.extend expects argument 0 to be an Object');
-		}
+        if (count > 1) {
+            for (var n = 1; n < count; n++) {
+                var argument = arguments[n];
+                for (var key in argument) {
+                    obj[key] = argument[key];
+                }
+            }
+        }
 
-		if (count > 1) {
-			for ( var n = 1; n < count; n++ ) {
-				var argument = arguments[n];
-				for ( var key in argument ) {
-					obj[key] = argument[key];
-				}
-			}
-		}
+        return obj;
 
-		return obj;
+    }
 
-	}
+    // export
 
-	// export
-
-	return jQuery;
+    return jQuery;
 
 }));
